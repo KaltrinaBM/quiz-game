046e2651bcb86d87e60b3c8fc6a941ff38d46e86
Kaltrina Mehmeti <bajramikaltrina@gmail.com>
2022-11-26 00:08:51 +0000

    Debugged and reseted the javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,35 @@ let score = 0;
 let time;
 const total_time = 30;
 let sec = total_time;
+let quizQuestions = [];
 
-document.addEventListener("DOMContentLoaded", function () {
+
+document.addEventListener("DOMContentLoaded", async function () {
+
+    // First load question from remote API
+    await getData();
+
+    // load the main game
+    loadGame();
+
+})
+
+function loadGame() {
     // EventListeners for question buttons
+    handleOptionsClick();
+
+    // Handling Event listner on button click
+    handleSubmit();
+
+    if (current_question <= 9) {
+        current_question++;
+        showQuestion(current_question);
+    } else {
+        showResult()
+    }
+}
+
+function handleOptionsClick() {
     let buttons = document.getElementsByClassName("options");
     for (let button of buttons) {
         button.addEventListener("click", function () {
@@ -22,101 +48,91 @@ document.addEventListener("DOMContentLoaded", function () {
             checkIfScore(questions);
         });
     }
+}
 
-    // EventListener for load next question button
-    
-    document.getElementById("submit-button").addEventListener("click", function () {
-        if (current_question < 9) {
-            current_question++;
-            showQuestion(current_question);
-        }
-    });
-
-    let quizQuestions = [];
+async function getData() {
     const URL = 'https://the-trivia-api.com/api/questions?categories=general_knowledge&limit=10'
-    async function getData() {
-        const response = await fetch(URL);
-        const data = await response.json();
-        quizQuestions = data;
-        console.log(quizQuestions);
-        showQuestion();
-    }
-    getData();
+    const response = await fetch(URL);
+    const data = await response.json();
+    quizQuestions = data;
+    console.log(quizQuestions);
+}
 
 
-    /**timer*/
-
-    function timer() {
-        time_element.innerHTML = sec;
-        sec--;
-        if (sec == 0) {
-            sec = total_time;
-            clearInterval(time);
-            checkIfScore();
-            current_question++;
-            showQuestion();
-        }
-    }
-
-    function checkIfScore() {
-        let optionIdSelected = document.querySelector('button[type="submit"], button:not([type])');
-
-
-        let option_correct = quizQuestions[current_question].correctAnswer;
-        if (optionIdSelected != null) {
-            if (optionIdSelected.id == option_correct) {
-                score++;
-            }
-        }
-    }
-
-    /**
-     * The function to show question and options on html page.
-     */
-
-    function showQuestion() {
+/**timer*/
+function timer() {
+    time_element.innerHTML = sec;
+    sec--;
+    if (sec == 0) {
         sec = total_time;
         clearInterval(time);
-        timer();
-        time = setInterval(timer, 1000);
-        document.getElementById("question-number").innerText = current_question + 1;
+        checkIfScore();
+        current_question++;
+        showQuestion();
+    }
+}
 
-        document.querySelectorAll('button[type="submit"], button:not([type])').forEach(option => option.checked = false)
+function checkIfScore() {
+    let optionIdSelected = document.querySelector('button[type="submit"], button:not([type])');
 
-        if (current_question >= quizQuestions.length) {
-            showResult();
-        }
 
-        //set questions and options from array
-
-        question.innerHTML = quizQuestions[current_question].question;
-        document.getElementById("options1").innerHTML =
-            quizQuestions[current_question].correctAnswer;
-        document.getElementById("options2").innerHTML =
-            quizQuestions[current_question].incorrectAnswers[0];
-        document.getElementById("options3").innerHTML =
-            quizQuestions[current_question].incorrectAnswers[1];
-        document.getElementById("options4").innerHTML =
-            quizQuestions[current_question].incorrectAnswers[2];
-
-        if (current_question === 9) {
-            document.getElementById("submit-button").innerHTML = "Get Result";
-        }
-
-        function shuffled(showQuestion) {
-            showQuestion = showQuestion.slice(); // shallow copy
-            for (var i = 0; i < showQuestion.length; i++) {
-                var j = Math.floor(Math.random() * (showQuestion.length - i)) + i;
-                [showQuestion[i], showQuestion[j]] = [showQuestion[j], showQuestion[i]]; // swap
-            }
-            return shuffled;
+    let option_correct = quizQuestions[current_question].correctAnswer;
+    if (optionIdSelected != null) {
+        if (optionIdSelected.id == option_correct) {
+            score++;
         }
     }
+}
 
-    /**
-     * Handling Event listner on button click
-     */
-    submit.addEventListener('submit-button', () => {
+/**
+ * The function to show question and options on html page.
+ */
+
+function showQuestion() {
+    sec = total_time;
+    clearInterval(time);
+    timer();
+    time = setInterval(timer, 1000);
+    document.getElementById("question-number").innerText = current_question;
+
+    document.querySelectorAll('button[type="submit"], button:not([type])').forEach(option => option.checked = false)
+
+    if (current_question >= quizQuestions.length) {
+        showResult();
+    }
+
+    //set questions and options from array
+
+    question.innerHTML = quizQuestions[current_question].question;
+    document.getElementById("options1").innerHTML =
+        quizQuestions[current_question].correctAnswer;
+    document.getElementById("options2").innerHTML =
+        quizQuestions[current_question].incorrectAnswers[0];
+    document.getElementById("options3").innerHTML =
+        quizQuestions[current_question].incorrectAnswers[1];
+    document.getElementById("options4").innerHTML =
+        quizQuestions[current_question].incorrectAnswers[2];
+
+    if (current_question === 9) {
+        document.getElementById("submit-button").innerHTML = "Get Result";
+    }
+
+    function shuffled(showQuestion) {
+        showQuestion = showQuestion.slice(); // shallow copy
+        for (var i = 0; i < showQuestion.length; i++) {
+            var j = Math.floor(Math.random() * (showQuestion.length - i)) + i;
+            [showQuestion[i], showQuestion[j]] = [showQuestion[j], showQuestion[i]]; // swap
+        }
+        return shuffled;
+    }
+}
+
+/**
+ * Handling Event listner on button click
+ */
+function handleSubmit() {
+    submit.addEventListener("click", (e) => {
+        e.preventDefault()
         checkIfScore();
         current_question++;
         if (current_question >= quizQuestions.length) {
@@ -126,9 +142,11 @@ document.addEventListener("DOMContentLoaded", function () {
             showQuestion();
         }
     });
-    function showResult() {
-        current_question = 0;
-        checkIfScore();
-        document.getElementById("score", score);
-    }
-})
+}
+
+function showResult() {
+    clearInterval(time);
+    current_question = 0;
+    checkIfScore();
+    document.getElementById("score", score);
+}
